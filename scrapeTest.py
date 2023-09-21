52a12a38c26f01de8846e3b065bac38cff9f92d2
Abdallah Elnahal <85466037+aelnahal10@users.noreply.github.com>
2023-09-21 16:10:50 +0000

    Final Version ScrapeTest

--- scrapeTest.py
+++ scrapeTest.py
@@ -1,35 +1,52 @@
 from googleapiclient.discovery import build
+from pytube import YouTube
 
-api_key = 'AIzaSyCd5vTBY66sbDamoFC_ZcxfjKVG0bqQqOw'
+# Setup for scraping
+api_key = 'AIzaSyCd5vTBY66sbDamoFC_ZcxfjKVG0bqQqOw'  # Replace with your API key
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 # pylint: disable=no-member
-request = youtube.search().list(
-    part='snippet',
-    maxResults=25,
-    type='video',
-    q='shorts AND (redpill|success|masculinity|goggins)',  # Query for Shorts with specific keywords
-    order='viewCount',  # Sorting by view count
-    relevanceLanguage='en'  # Looking for videos in English
-)
+def fetch_video_links():
+    request = youtube.search().list(
+        part='snippet',
+        maxResults=25,
+        type='video',
+        q='shorts AND (redpill|masculinity|goggins|stay hard)',
+        order='viewCount',
+        relevanceLanguage='en'
+    )
 
+    response = request.execute()
 
-response = request.execute()
+    video_list = []
+    for item in response['items']:
+        video_id = item['id']['videoId']
+        video_name = item['snippet']['title']
+        video_description = item['snippet']['description']
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+        
+        video_list.append({
+            'name': video_name,
+            'description': video_description,
+            'url': video_url
+        })
 
-video_list = []
-for item in response['items']:
-    video_id = item['id']['videoId']
-    video_name = item['snippet']['title']
-    video_description = item['snippet']['description']
-    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    return video_list
+
+def download_video(video_url, video_name):
+    yt = YouTube(video_url)
+    video_stream = yt.streams.get_highest_resolution()
+    video_path = f"./{video_name}.mp4"  # Save with video name as filename
+    print(f"Downloading {yt.title}...")
+    video_stream.download(output_path=video_path)
+    print(f"{yt.title} Download complete!")
+
+def main():
+    video_list = fetch_video_links()
     
-    video_list.append({
-        'name': video_name,
-        'description': video_description,
-        'url': video_url
-    })
+    for video in video_list:
+        print(f"Name: {video['name']}, Description: {video['description']}, URL: {video['url']}")
+        download_video(video['url'], video['name'])
 
-for video in video_list:
-    print(f"Name: {video['name']}, Description: {video['description']}, URL: {video['url']}")
-
-
+if __name__ == "__main__":
+    main()
